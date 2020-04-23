5f0d39444506ede7a3f980fccdf8e686e58bf0ab
Alan <48613814+00ffffcoder@users.noreply.github.com>
2020-04-23 11:27:37 +0800

    update
    update

--- js/comment-2.js
+++ js/comment-2.js
@@ -1,11 +1,12 @@
 window.onload = function () {
   // 功能1： 点击按钮，删除评论
   var delBtn = document.getElementsByTagName("button");
-  // console.log(delBtn);
+  var ul = document.getElementById("ul_con");
+  var h2 = document.getElementsByTagName("h2")[0];
+  var form = document.getElementsByTagName("form")[0];
 
   for (var i = 0; i < delBtn.length; i++) {
     delBtn[i].onclick = delComment;
-
   }
 
   function delComment() {
@@ -14,10 +15,14 @@ window.onload = function () {
     //获取要删除的项目名字
     var delName = li.children[0].innerHTML;
     //弹出提示确认框
-    var warning = confirm("确定要删除 " + delName + " 吗？");
+    var warning = confirm("确定要删除 " + delName + " 的评论吗？");
     //确认后，删除
     if (warning) {
-      li.parentNode.removeChild(li);
+      ul.removeChild(li);
+    }
+    console.log(h2);
+    if (ul.children.length===0){
+      h2.style.display = "block";
     }
   }
 
@@ -25,14 +30,14 @@ window.onload = function () {
   document.getElementById("subBtn").onclick = function (){
 
     //获取输入的文本内容
-    var userName = document.getElementById("nameInput").value;
-    var commentText = document.getElementById("textInput").value;
+    var newUserName = document.getElementById("nameInput").value;
+    var newCommentText = document.getElementById("textInput").value;
 
 
     //创建一个li
     var newLi = document.createElement("li");
-    newLi.innerHTML = "<p class='p1_user'>" +  userName  + "：</p>"+
-                      "<p class='p2_text'>" + commentText + "</p>"+
+    newLi.innerHTML = "<p class='p1_user'>" +  newUserName  + "：</p>"+
+                      "<p class='p2_text'>" + newCommentText + "</p>"+
                       "<button class='del_btn'>删 除</button>";
 
     //对于新添加的li , 也可以实现功能1
@@ -40,9 +45,10 @@ window.onload = function () {
 
     var ul = document.getElementById("ul_con");
     //把 li 添加到 ul 中
-    var submitComment = confirm("确定要发表评论 " + commentText + " 吗？");
+    var submitComment = confirm("确定要发表评论[ " + newCommentText + " ]吗？");
     if (submitComment) {
       ul.appendChild(newLi);
+      form.reset();
     }
 
 
